813ece510f0567d3632907d5b2836e5037bdcb63
tszhong0411 <me@honghong.me>
2025-02-22 16:23:26 +0800

    feat: ep6 pipes

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,4 +1,9 @@
+import { IsEnum, MinLength } from 'class-validator';
+
 export class CreateNinjaDto {
+  @MinLength(3)
   name: string;
+
+  @IsEnum(['stars', 'sword'], { message: 'Use correct weapon!' })
   weapon: 'stars' | 'sword';
 }
